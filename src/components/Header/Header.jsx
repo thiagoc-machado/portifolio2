96fa72b82ee24e850702a1d7b56b1b5df84bef14
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-09-19 10:55:24 +0200

    hovers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -48,19 +48,19 @@ const Header = () => {
               </a>
             </li>
             <li className='nav__item'>
-              <a href='#services' onClick={() => setActiveNav('#services')}
-                className={
-                  activeNav === '#services' ? 'nav__link active-link' : 'nav__link'
-                }>
-                <i className='uil uil-briefcase-alt nav__icon'></i>Services
-              </a>
-            </li>
-            <li className='nav__item'>
               <a href='#qualification' onClick={() => setActiveNav('#portfolio')}
                 className={
                   activeNav === '#portfolio' ? 'nav__link active-link' : 'nav__link'
                 }>
-                <i className='uil uil-scenery nav__icon'></i>Portfolio
+                <i className='uil uil-scenery nav__icon'></i>Qualifications
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#services' onClick={() => setActiveNav('#services')}
+                className={
+                  activeNav === '#services' ? 'nav__link active-link' : 'nav__link'
+                }>
+                <i className='uil uil-briefcase-alt nav__icon'></i>Projects
               </a>
             </li>
             <li className='nav__item'>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,54 +32,32 @@ const Contact = () => {
       <h2 className='section__title '>Get in touch</h2>
       <span className='section__subtitle'>Contact me</span>
       <div className='contact__container container grid'>
-        <div className='contact__content'>
-          {/* <h3 className='contact__title'>Talk to me</h3> */}
+      <div className='contact__content'>
           <br></br>
 
           <div className='contact__info'>
-            <div className='contact__card'>
-              <i className='bx bx-mail-send contact__card-icon'></i>
-
-              <h3 className='contact__card-title'>Email</h3>
-              <span className='contact__card-data'>
-                thiagocmach@gmail.com
-              </span>
-
-              <a
-                href='mailto:thiagocmach@gmail.com'
-                className='contact__button'
-              >
-                Write me{''}
-                <i className='bx bx-right-arrow-alt contact__button-icon'></i>
+              <a href='mailto:thiagocmach@gmail.com' className='contact__card-link'>
+                  <div className='contact__card'>
+                      <i className='bx bx-mail-send contact__card-icon'></i>
+                      <h3 className='contact__card-title'>Email</h3>
+                      <span className='contact__card-data'>thiagocmach@gmail.com</span>
+                  </div>
               </a>
-            </div>
-            <div className='contact__card'>
-              <i className='bx bxl-linkedin contact__card-icon'></i>
-
-              <h3 className='contact__card-title'>Linkedin</h3>
-              <span className='contact__card-data'></span>
-
-              <a
-                href='https://www.linkedin.com/in/thiagomach/'
-                className='contact__button'
-              >
-                Write me{''}
-                <i className='bx bx-right-arrow-alt contact__button-icon'></i>
+              <a href='https://www.linkedin.com/in/thiagomach/' className='contact__card-link'>
+                  <div className='contact__card'>
+                      <i className='bx bxl-linkedin contact__card-icon'></i>
+                      <h3 className='contact__card-title'>Linkedin</h3>
+                      <span className='contact__card-data'>linkedin.com/in/thiagomach</span>
+                  </div>
               </a>
-            </div>
-
-            <div className='contact__card'>
-              <i className='bx bxl-instagram contact__card-icon'></i>
-
-              <h3 className='contact__card-title'>Instagram</h3>
-              <span className='contact__card-data'>Thiago</span>
-
-              <a href='https://www.instagram.com/thiagoc.machado/' className='contact__button'>
-                Write me{''}
-                <i className='bx bx-right-arrow-alt contact__button-icon'></i>
+              <a href='https://www.instagram.com/thiagoc.machado/' className='contact__card-link'>
+                  <div className='contact__card'>
+                      <i className='bx bxl-instagram contact__card-icon'></i>
+                      <h3 className='contact__card-title'>Instagram</h3>
+                      <span className='contact__card-data'>instagram.com/thiagoc.machado</span>
+                  </div>
               </a>
-            </div>
-          </div>
+          </div>     
         </div>
         <div className='contact__content'>
           <h3 className='contact__title'>Write your message</h3>
